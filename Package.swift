4c1c6eb9c83424a64d3ead45a50fb7bbbb25995f
Jorge Palacio <jorge.palacio@klarna.com>
2025-06-30 23:03:53 +0200

    Changed urls for github ones.

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KlarnaMobileSDK",
-            url: "https://artifactory.klarna.net/artifactory/l-generic-mobilesdk-releases/klarna/feature/ios/sdk/2025-06-26/1514-1d952fb3-release/KlarnaMobileSDK/universal/KlarnaMobileSDK.xcframework.zip",
+            url: "https://github.com/jorgepalac1o/klarna-ios/releases/download/3.0.0/KlarnaMobileSDK.xcframework.zip",
             checksum: "4682d9e7f10bfe93e71b31aa9f7a8f59f6e43606f575d5b28fa245307d6f66c5"
         ),
         .binaryTarget(
             name: "KlarnaCore",
-            url: "https://artifactory.klarna.net/artifactory/l-generic-mobilesdk-releases/klarna/feature/ios/sdk/2025-06-26/1514-1d952fb3-release/KlarnaCore/universal/KlarnaCore.xcframework.zip",
+            url: "https://github.com/jorgepalac1o/klarna-ios/releases/download/3.0.0/KlarnaCore.xcframework.zip",
             checksum: "e73ca12cacd534aba3bec60e8be847ef0f4ff08efc95d794e15eb1194de72ae3"
         ),
     ]
